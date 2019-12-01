7139844b3aa392c9928a43d527d617e7cbebd286
alexbispo <alexbispo.artes@gmail.com>
2019-11-30 22:22:36 -0300

    Created RandomWords widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,9 +10,22 @@ class MyApp extends StatelessWidget {
         title: 'Welcome to Flutter',
         home: Scaffold(
           appBar: AppBar(title: const Text('Welcome to Flutter')),
-          body: const Center(
-            child: const Text('Hello World'),
+          body: Center(
+            child: RandomWords(),
           ),
         ));
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState()  => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    var wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
