a5f485c07ec4d5610f87b2e7150ed609dc5e76b2
alexbispo <alexbispo.artes@gmail.com>
2019-11-30 23:45:34 -0300

    Created infinite list of startup name suggestions widget.

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,7 @@ class MyApp extends StatelessWidget {
         title: 'Welcome to Flutter',
         home: Scaffold(
           appBar: AppBar(title: const Text('Welcome to Flutter')),
-          body: Center(
-            child: RandomWords(),
-          ),
+          body: RandomWords()
         ));
   }
 }
@@ -23,9 +21,38 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = TextStyle(fontSize: 18);
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i) {
+        if(i.isOdd) {
+          return Divider();
+        }
+
+        final int index = i ~/ 2;
+
+        if(index >= _suggestions.length){
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    var wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return _buildSuggestions();
   }
 }
