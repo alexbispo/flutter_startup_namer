b939a91cd49a90b3d39057b1153f7f7213f79b30
alexbispo <alexbispo.artes@gmail.com>
2019-12-02 20:04:47 -0300

    Adding heart icons at list items.

--- lib/main.dart
+++ lib/main.dart
@@ -23,12 +23,18 @@ class RandomWords extends StatefulWidget {
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
   final TextStyle _biggerFont = TextStyle(fontSize: 18);
+  final Set<WordPair> _saved = Set<WordPair>();
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
     );
   }
